011d54abda6178fbf272c50992820060fbf823c2
Miriam Sparbrod <miriam.sparbrod@gmail.com>
2024-07-30 17:21:15 +0200

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "tnendkignywadpoizczb.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/cabin-images/**",
+      },
+    ],
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
